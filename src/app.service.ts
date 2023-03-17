597ca69bc355f4bace0d9d6f44df74dfca71916f
baizhi958216 <1475289190@qq.com>
2023-03-18 07:52:13 +0800

    app: Custom index prompt
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,16 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `
+    <div style="
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;"
+    >
+      <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+      <p>Senser Logger live data via http<p>
+    </div>`;
   }
 }
